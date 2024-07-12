837d3773677c0c897bf367339918394590ebb6e3
mockts <167064448+mockts@users.noreply.github.com>
2024-07-12 19:12:35 +0900

    Title: 최대 2번의 연산, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 240712/최대 2번의 연산/up-to-2-calculations.py
+++ 240712/최대 2번의 연산/up-to-2-calculations.py
@@ -1,11 +1,10 @@
-# 정수 a 입력받기
-a = int(input().strip())
+# 변수 선언 및 입력
+a = int(input())
 
-# 주어진 규칙에 따라 계산
 if a % 2 == 0:
-    result = a // 2
-else:
-    result = (a + 1) // 2
+    a //= 2
+    
+if a % 2 == 1:
+    a = (a + 1) // 2
 
-# 최종 결과 출력
-print(result)
+print(a)
