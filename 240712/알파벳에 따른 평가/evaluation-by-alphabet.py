6c22b82bf6d173803e9ca8d9b292f2d092372517
mockts <167064448+mockts@users.noreply.github.com>
2024-07-12 18:57:02 +0900

    Title: 알파벳에 따른 평가, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240712/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+letter = input().strip()  
+
+if letter == 'S':
+    print("Superior")
+elif letter == 'A':
+    print("Excellent")
+elif letter == 'B':
+    print("Good")
+elif letter == 'C':
+    print("Usually")
+elif letter == 'D':
+    print("Effort")
+else:
+    print("Failure")
