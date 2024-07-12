ac362d1c1816844c23115b7771985d29e0c2b698
mockts <167064448+mockts@users.noreply.github.com>
2024-07-12 19:04:01 +0900

    Title: 비교 연산, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240712/비교 연산/comparison-operator.py
@@ -0,0 +1,37 @@
+a, b = map(int, input().split())
+
+# a가 b보다 같거나 큰지 여부
+if a >= b:
+    print(1)
+else:
+    print(0)
+
+# a가 b보다 큰지 여부
+if a > b:
+    print(1)
+else:
+    print(0)
+
+# b가 a보다 같거나 큰지 여부
+if b >= a:
+    print(1)
+else:
+    print(0)
+
+# b가 a보다 큰지 여부
+if b > a:
+    print(1)
+else:
+    print(0)
+
+# a와 b가 같은지 여부
+if a == b:
+    print(1)
+else:
+    print(0)
+
+# a와 b가 다른지 여부
+if a != b:
+    print(1)
+else:
+    print(0)
