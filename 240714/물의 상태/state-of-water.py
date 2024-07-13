43777166e9c1d3e6edba0c43c414c2d5c6fc11a0
mockts <167064448+mockts@users.noreply.github.com>
2024-07-14 00:50:20 +0900

    Title: 물의 상태, Time: 160ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240714/물의 상태/state-of-water.py
@@ -0,0 +1,13 @@
+# if : 만약 ..라면
+# elif : 그렇지 않고 ...이라면 (조건추가 가능)
+# else : 아니면 (조건 뒤에 x)
+
+
+n = int(input())
+
+if n < 0 :
+    print("ice")
+elif n >=100 :
+    print("vapor")
+else :
+    print("water")
