f5b72c33757c1785a4dea2f4eb269822d14aa4eb
mockts <167064448+mockts@users.noreply.github.com>
2024-07-14 01:43:21 +0900

    Title: 남녀노소 구분짓기, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

--- 240714/남녀노소 구분짓기/sex-and-age.py
+++ 240714/남녀노소 구분짓기/sex-and-age.py
@@ -1,28 +1,14 @@
 sex = int(input())
 age = int(input())
 
-if sex == 0 and age>= 19:
-    print("MAN")
-elif sex == 1:
-    print("WOMAN")
+if sex == 0 :
+    if age>= 19:
+        print("MAN")
+    else : 
+        print("BOY")
 
-if sex == 1 and age<= 19:
-    print("BOY")
-elif sex == 1:
-    print("GIRL")
-
-
-    
-# gender = int(input())
-# age = int(input())
-
-# if gender == 0:
-#     if age >= 19:
-#         print("MAN")
-#     else:
-#     	print("BOY")
-# else:
-#     if age >= 19:
-#         print("WOMAN")
-#     else:
-#         print("GIRL")
+if sex == 1 :
+    if age>= 19:
+        print("WOMAN")
+    else :
+        print("GIRL")
