d045665a7b36945a4cf19bf362a806699400d670
mockts <167064448+mockts@users.noreply.github.com>
2024-07-18 18:37:13 +0900

    Title: 행과 열, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 240718/행과 열/row-column.py
+++ 240718/행과 열/row-column.py
@@ -1,5 +1,8 @@
-n = int(input())
-for i in range(n, 0, -1):
-    for j in range(n, 0, -1):
-        print(f'({i},{j})', end=' ')
-    print()
+a, b = map(int, input().split())
+
+for i in range(1, a + 1):
+    for j in range(1, b + 1):
+        if j == b:
+            print(i * j)
+        else:
+            print(i * j, end=" ")
