1044f4c85056851c913537158fa46b7f71077edf
mockts <167064448+mockts@users.noreply.github.com>
2024-07-18 18:26:00 +0900

    Title: 별표 출력하기 7, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240718/별표 출력하기 7/print-star-7.py
+++ 240718/별표 출력하기 7/print-star-7.py
@@ -1,8 +1,5 @@
 n = int(input())
 for i in range(n, 0, -1):
     for j in range(i):
-        if j == 0 or j == i - 1 or i == n:
-            print('* ', end='')
-        else:
-            print('  ', end='')
+        print('* ', end='')
     print()
