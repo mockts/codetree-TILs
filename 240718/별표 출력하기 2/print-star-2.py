fa95898b66b129eaacee080e9d605a5895890e41
mockts <167064448+mockts@users.noreply.github.com>
2024-07-18 18:24:50 +0900

    Title: 별표 출력하기 2, Time: 99ms, Memory: 24MB, Status: Passed - CodeTree

--- 240718/별표 출력하기 2/print-star-2.py
+++ 240718/별표 출력하기 2/print-star-2.py
@@ -1,9 +1,7 @@
-def star(n):
-    for i in range(n, 0, -1):
-        for j in range(i):
-            print('* ', end='')
-        print()
-
-# Example usage:
 n = int(input())
-star(n)
+
+
+for i in range(n):
+	for _ in range(n-i):
+		print("*", end=" ")
+	print()
