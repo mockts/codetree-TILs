8f70e6b3cad1c663eef18468af89eda643a8cb11
mockts <167064448+mockts@users.noreply.github.com>
2024-07-18 18:33:09 +0900

    Title: 별표 출력하기 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240718/별표 출력하기 3/print-star-3.py
+++ 240718/별표 출력하기 3/print-star-3.py
@@ -1,25 +1,9 @@
 n = int(input())
 
+.
 for i in range(n):
-
-    for j in range(i):
-        print(' ', end=' ')
-
-    for j in range(n - i):
-        if j == n - i - 1:
-            print('*', end='')
-        else:
-            print('* ', end='')
-    print()
-
-for i in range(1, n):
-
-    for j in range(n - i - 1):
-        print(' ', end=' ')
-
-    for j in range(i + 1):
-        if j == i:
-            print('*', end='')
-        else:
-            print('* ', end='')
-    print()
+	for _ in range(i):
+		print(" ", end=" ")
+	for _ in range((2 * n) - (2 * i) - 1):
+		print("*", end=" ")
+	print()
