2b43783c1369fec2dfad095767c8f977d2f5bc77
mockts <167064448+mockts@users.noreply.github.com>
2024-07-22 18:30:59 +0900

    Title: 특이한 출력, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240722/특이한 출력/special-print.py
+++ 240722/특이한 출력/special-print.py
@@ -1,9 +1,10 @@
+# 변수 선언 및 입력
 n = int(input())
-
-for x in range(1, n + 1):
-    for y in range(1, n + 1):
-        print(f"({x}, {y})", end=' ')
-        if (x + y) % 4 == 0:
-            print()
-    if (x + y) % 4 != 0:
-        print()
+	
+# 특이한 조건대로 구구단을 출력합니다.
+for i in range(1, n + 1):
+	for j in range(1, n + 1):
+		if (i + j) % 4 == 0:
+			print(f"({i}, {j})")
+		else:
+			print(f"({i}, {j})", end=" ")
