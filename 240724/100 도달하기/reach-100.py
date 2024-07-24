8ec86f006f7c848e24574f8dbc2d396001008c4c
mockts <167064448+mockts@users.noreply.github.com>
2024-07-24 18:52:07 +0900

    Title: 100 도달하기, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240724/100 도달하기/reach-100.py
+++ 240724/100 도달하기/reach-100.py
@@ -1,14 +1,15 @@
+# 입력받은 정수를 배열의 두번째 인덱스에 저장
 n = int(input())
+arr = [1, n]
+cnt = 1
 
-first = 1
-second = n
-sequence = [first, second]
-
+# 배열을 전전항과 전항을 더한 수로 채워나가고, 그 값이 100을 초과하면 while문을 탈출
 while True:
-    next_value = first + second
-    if next_value > 120:
-        break
-    sequence.append(next_value)
-    first, second = second, next_value
+	cnt += 1
+	arr.append(arr[cnt - 1] + arr[cnt - 2])
+	if arr[cnt] > 100:
+		break
 
-print(' '.join(map(str, sequence)))
+# 100을 딱 초과한 항까지 배열의 인덱스에 저장된 값을 출력
+for elem in arr:
+	print(elem, end=" ")
