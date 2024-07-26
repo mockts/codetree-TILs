25cf5018c2f72bc1cc0c2571ab26cf2acb495137
mockts <167064448+mockts@users.noreply.github.com>
2024-07-26 20:48:34 +0900

    Title: 특정 문자의 유무, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240726/특정 문자의 유무/specific-character-presence.py
+++ 240726/특정 문자의 유무/specific-character-presence.py
@@ -3,9 +3,9 @@ s = input()
 exists_ee = "No" 
 exists_ab = "No"          
 for i in range(len(s)- 1):
-    if s[i:i+2] == 'ab':
-        exists_ee = 'Yes'
     if s[i:i+2] == 'ee':
         exists_ab = 'Yes'
+    if s[i:i+2] == 'ab':
+        exists_ee = 'Yes'
 
 print(exists_ee, exists_ab)
