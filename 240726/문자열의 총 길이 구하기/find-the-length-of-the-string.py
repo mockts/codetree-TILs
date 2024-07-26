63cbc73a5a97d912076c9b0078fd5026576ef11c
mockts <167064448+mockts@users.noreply.github.com>
2024-07-26 19:57:36 +0900

    Title: 문자열의 총 길이 구하기, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240726/문자열의 총 길이 구하기/find-the-length-of-the-string.py
+++ 240726/문자열의 총 길이 구하기/find-the-length-of-the-string.py
@@ -2,9 +2,14 @@
 # a,b,c = tuple(input().split())
 # 위는 map(str, input().split())과 동일
 
+# 단어가 10개면 공백은 9개
 arr = input().split()
 
 len_val = 0
 for x in arr :
     len_val +=len(x)
 print(len_val)
+
+
+# s = input
+# print(len[s]-9)
